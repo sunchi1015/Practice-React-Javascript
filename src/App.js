9da15d595e1a7417eff20813ff47070c89b2607d
sunchi1015 <67795465+sunchi1015@users.noreply.github.com>
2020-07-12 13:40:27 +0530

    functional component with state using hooks - useState()

--- src/App.js
+++ src/App.js
@@ -1,45 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './styles.css';
 import Person from './Component/Person';
 
-// export default function App() {
-//   return (
-//     <div className="App">
-//       <Person name={"Jogn"} age={32} />
-//       <Person name={"Jogn"} age={32}>
-//         <h4>Childer From App</h4>
-//         <h4>Childer From To Custom</h4>
-//       </Person>
-//     </div>
-//   );
-// }
-
-class App extends Component {
-  state = {
+export default function App() {
+  let [prevState, stateChange] = useState({
     Person: [{ name: 'John', age: 37 }, { name: 'Bake', age: 12 }],
     showPerson: true
-  };
-
-  changeName = () => {
-    this.setState({
-      Person: [{ name: 'John Calvin', age: 37 }, { name: 'Bake', age: 12 }],
-      showPerson: true
+  });
+  const changeName = () => {
+    stateChange({
+      Person: [{ name: 'John Calvin', age: 37 }, { name: 'Bake', age: 12 }]
     });
-    console.log(this.state);
+    console.log(prevState);
   };
 
-  render() {
-    return (
-      <div className="App">
-        <button onClick={this.changeName}>Change Name</button>
-        <Person name={this.state.Person[0].name} age={this.state.Person[0].age} />
-        <Person name={'Jogn'} age={32}>
-          <h4>Childer From App</h4>
-          <h4>Childer From To Custom</h4>
-        </Person>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <button onClick={changeName}>Change Name</button>
+      <Person name={prevState.Person[0].name} age={prevState.Person[0].age} />
+      <Person name={prevState.Person[1].name} age={prevState.Person[1].age}>
+        <h4>Childer From App</h4>
+        <h4>Childer From To Custom</h4>
+      </Person>
+    </div>
+  );
 }
 
-export default App;
+// class App extends Component {
+//   state = {
+//     Person: [{ name: 'John', age: 37 }, { name: 'Bake', age: 12 }],
+//     showPerson: true
+//   };
+
+//   changeName = () => {
+//     this.setState({
+//       Person: [{ name: 'John Calvin', age: 37 }, { name: 'Bake', age: 12 }],
+//       showPerson: true
+//     });
+//     console.log(this.state);
+//   };
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <button onClick={this.changeName}>Change Name</button>
+//         <Person name={this.state.Person[0].name} age={this.state.Person[0].age} />
+//         <Person name={'Jogn'} age={32}>
+//           <h4>Childer From App</h4>
+//           <h4>Childer From To Custom</h4>
+//         </Person>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
