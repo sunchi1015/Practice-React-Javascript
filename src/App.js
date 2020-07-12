d56b89bd3350636a742cd2346f98ad6b71af9453
sunchi1015 <67795465+sunchi1015@users.noreply.github.com>
2020-07-12 17:03:48 +0530

    section 3 completed and section 4 1 video covered

--- src/App.js
+++ src/App.js
@@ -2,56 +2,127 @@ import React, { Component, useState } from 'react';
 import './styles.css';
 import Person from './Component/Person';
 
-export default function App() {
-  let [prevState, stateChange] = useState({
-    Person: [{ name: 'John', age: 37 }, { name: 'Bake', age: 12 }],
-    showPerson: true
-  });
-  const changeName = () => {
-    stateChange({
-      Person: [{ name: 'John Calvin', age: 37 }, { name: 'Bake', age: 12 }]
-    });
-    console.log(prevState);
-  };
-
-  return (
-    <div className="App">
-      <button onClick={changeName}>Change Name</button>
-      <Person name={prevState.Person[0].name} age={prevState.Person[0].age} />
-      <Person name={prevState.Person[1].name} age={prevState.Person[1].age}>
-        <h4>Childer From App</h4>
-        <h4>Childer From To Custom</h4>
-      </Person>
-    </div>
-  );
-}
-
-// class App extends Component {
-//   state = {
+// export default function App() {
+//   let [prevState, stateChange] = useState({
 //     Person: [{ name: 'John', age: 37 }, { name: 'Bake', age: 12 }],
 //     showPerson: true
-//   };
-
-//   changeName = () => {
-//     this.setState({
-//       Person: [{ name: 'John Calvin', age: 37 }, { name: 'Bake', age: 12 }],
-//       showPerson: true
+//   });
+//   const changeName = () => {
+//     stateChange({
+//       Person: [{ name: 'John Calvin', age: 37 }, { name: 'Bake', age: 12 }]
 //     });
-//     console.log(this.state);
+//     console.log(prevState);
 //   };
 
-//   render() {
-//     return (
-//       <div className="App">
-//         <button onClick={this.changeName}>Change Name</button>
-//         <Person name={this.state.Person[0].name} age={this.state.Person[0].age} />
-//         <Person name={'Jogn'} age={32}>
-//           <h4>Childer From App</h4>
-//           <h4>Childer From To Custom</h4>
-//         </Person>
-//       </div>
-//     );
-//   }
+//   return (
+//     <div className="App">
+//       <button onClick={changeName}>Change Name</button>
+//       <Person name={prevState.Person[0].name} age={prevState.Person[0].age} />
+//       <Person name={prevState.Person[1].name} age={prevState.Person[1].age}>
+//         <h4>Childer From App</h4>
+//         <h4>Childer From To Custom</h4>
+//       </Person>
+//     </div>
+//   );
 // }
 
-// export default App;
+class App extends Component {
+  // style = {};
+  state = {
+    Person: [{ name: 'John', age: 37 }, { name: 'Bake', age: 12 }],
+    showPerson: true
+  };
+
+  changeNameNormal = newName => {
+    console.log('new Name', newName);
+    this.setState({
+      Person: [{ name: newName, age: 23 }, { name: 'Bake', age: 12 }],
+      showPerson: true
+    });
+    console.log(this.state);
+  };
+
+  changeName = event => {
+    console.log('event', event.target.value);
+    const newAge = event.target.value;
+    this.setState({
+      Person: [{ name: 'John Calvin', age: newAge }, { name: 'Bake', age: 12 }],
+      showPerson: true
+    });
+    console.log(this.state);
+  };
+
+  toggleData = () => {
+    const currentState = this.state.showPerson;
+    this.setState({ showPerson: !currentState });
+    console.log('full sate--->', this.state);
+  };
+  style = {
+    backgroundColor: 'coral',
+    font: 'inherit',
+    color: '#fff',
+    border: '1px solid #000',
+    padding: '1rem',
+    margin: '2rem 1rem',
+    outline: 'none',
+    cursor: 'pointer',
+    boxShadow: '2px 2px 10px 0 rgba(0,0,0,.4)'
+  };
+
+  render() {
+    // this.style = {
+    //   backgroundColor: 'coral',
+    //   font: 'inherit',
+    //   color: '#fff',
+    //   border: '1px solid #000',
+    //   padding: '1rem',
+    //   margin: '2rem 1rem',
+    //   outline: 'none',
+    //   cursor: 'pointer',
+    //   boxShadow: '2px 2px 10px 0 rgba(0,0,0,.4)'
+    // };
+    const style = {
+      backgroundColor: 'coral',
+      font: 'inherit',
+      color: '#fff',
+      border: '1px solid #000',
+      padding: '1rem',
+      margin: '2rem 1rem',
+      outline: 'none',
+      cursor: 'pointer',
+      boxShadow: '2px 2px 10px 0 rgba(0,0,0,.4)'
+    };
+    return (
+      <div className="App">
+        {/* Pass method params 1 */}
+        {/* style using class property */}
+        {/* <button style={this.style} onClick={event => this.changeNameNormal('Durga')}>
+          Change Name
+        </button> */}
+        <button style={this.style} onClick={this.toggleData}>
+          Toggle Data
+        </button>
+        {/* style using constant */}
+        <button style={style} onClick={event => this.changeNameNormal('Durga')}>
+          Change Name
+        </button>
+        {/* Pass method params 1 */}
+        <button style={style} onClick={this.changeNameNormal.bind(this, 'Arun')}>
+          Change Name2
+        </button>
+        {/* Pass method params 1 with Handle event from child*/}
+        {this.state.showPerson ? (
+          <div>
+            <Person changeAge={this.changeName} name={this.state.Person[0].name} age={this.state.Person[0].age} />
+            <Person name={this.state.Person[1].name} age={this.state.Person[1].age}>
+              <h4>Childer From App</h4>
+              <h4>Childer From To Custom</h4>
+            </Person>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/Component/Person.js
+++ src/Component/Person.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import '../Component/Person.css';
 const Person = props => {
   return (
-    <div>
+    <div className="Person">
       <h1>Employee Details</h1>
       <h3>Name:{props.name}</h3>
-      <h3>Age:{props.age}</h3>
+      <h3 onClick={props.changeAge}>Age:{props.age}</h3>
+      <input type="text" onChange={props.changeAge} value={props.age} />
       {props.children}
     </div>
   );
